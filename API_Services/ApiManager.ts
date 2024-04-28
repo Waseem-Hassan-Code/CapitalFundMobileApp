48a77c6bcb815f8cb34bd96070b1ab20599449ac
awais qadir <awaisqadir0620370@example.com>
2024-04-28 09:53:27 -0700

    image fixed

--- API_Services/ApiManager.ts
+++ API_Services/ApiManager.ts
@@ -1,10 +1,10 @@
 import { Platform } from "react-native";
 import axios from "axios";
 
-let baseURL = "http://192.168.10.12:5180";
+let baseURL = "http://192.168.67.42:5181";
 
 if (Platform.OS === "ios") {
-  baseURL = "http://192.168.10.12:5180";
+  baseURL = "http://192.168.67.42:5181";
 }
 
 const ApiManager = axios.create({
--- screens/navigator/DrawerNavigation.js
+++ screens/navigator/DrawerNavigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, View, Pressable, Image, Text } from 'react-native';
+import { Button, View, Pressable, Image, Text, TouchableOpacity,StyleSheet } from 'react-native';
 import { DrawerItemList, createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -10,10 +10,21 @@ import PaymentHistory from "../user/PaymentHistory";
 import MyComplaints from "../user/MyComplaints";
 import NewComplaint from "../user/NewComplaint";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import Toast from 'react-native-simple-toast';
+import { deleteToken } from "../../API_Services/Token";
 
 
 const Drawer = createDrawerNavigator();
-const DrawerNavigation = () => {
+const DrawerNavigation = ({navigation}) => {
+
+    const Logout=async()=>{
+        Toast.show('Logout');
+         deleteToken();
+        navigation.navigate('Login');
+        
+    }  
+
+
     return (
         <Drawer.Navigator initialRouteName="Home"
 
@@ -37,11 +48,19 @@ const DrawerNavigation = () => {
                                 }}>Capital Fund</Text>
                             </View>
                             <DrawerItemList {...props} />
+                            <TouchableOpacity style={styles.logoutBtn}
+                            onPress={Logout}>
+                                <View style={styles.row}>
+                                    <Icon name="logout" size={30} color={COLORS.secondary} />
+                                    <Text style={styles.logoutText}>Logout</Text>
+                                </View>
+
+                            </TouchableOpacity>
                         </SafeAreaView>
                     )
                 }
             }
-
+  
 
             screenOptions={{
                 //  headerShown:false,
@@ -130,5 +149,19 @@ const DrawerNavigation = () => {
 }
 export default DrawerNavigation;
 
-
+const styles = StyleSheet.create({
+    logoutBtn: {
+        width: '100%', alignItems: 'center', justifyContent: 'center',
+        borderWidth: 0.4, borderColor: COLORS.primary,
+        paddingVertical: 5
+    },
+    row: {
+        flexDirection: 'row', alignItems: 'center',
+        justifyContent: 'flex-start', width: '100%',
+        paddingHorizontal: 20
+    },
+    logoutText: {
+        fontSize: 18, fontWeight: 'bold', marginLeft: 20, color: COLORS.primary
+    }
+})
 
